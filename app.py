d42ee1c82507a07583c2f304fe6a97b2eac41ac9
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2023-03-25 00:41:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("chatbot.html")
 
 if __name__ == "__main__":
     app.run()
