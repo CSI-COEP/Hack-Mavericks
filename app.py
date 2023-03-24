9e8f4376068f20dc13343c8ec17575dc4c840bd3
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2023-03-25 00:44:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Code for accessing the HTML file
 @app.route('/')
 def home():
     return render_template("chatbot.html")
+
+# Code for getting message from user 
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    bot_response = str(chatbot.get_response(user_text))
+    return bot_response
 
 if __name__ == "__main__":
     app.run()
