7ddf9afefdbe7e4ae7310f24c4478305510cb43d
Anwesh Shaw <83204279+AnweshShaw@users.noreply.github.com>
2023-03-25 01:07:16 +0530

    Update accuracy.py

--- accuracy.py
+++ accuracy.py
@@ -4,6 +4,8 @@ from sklearn.metrics import accuracy_score
 
 X = data.drop(['Class'],axis=1)
 y = data['Class']
+# Splitting the dataset into a training set of 80% and tesing set of 20%.
+X_train, X_test, y_train,y_test = train_test_split(X,y,test_size=0.20,random_state = 41)
 
 # Random Forest Algorithm
 from sklearn.ensemble import RandomForestClassifier
